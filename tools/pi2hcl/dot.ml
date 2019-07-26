c1698c26bfcb6d099d437a1dce3af4439701e92a
jserot <jocelyn.serot@uca.fr>
2019-07-26 17:40:51 +0200

    First version of [tools/pi2hcl]

---
+++ tools/pi2hcl/dot.ml
@@ -0,0 +1,98 @@
+(**********************************************************************)
+(*                                                                    *)
+(*              This file is part of the HOCL package                 *)
+(*                                                                    *)
+(*  Copyright (c) 2019-present, Jocelyn SEROT (jocelyn.serot@uca.fr). *)
+(*                     All rights reserved.                           *)
+(*                                                                    *)
+(*  This source code is licensed under the license found in the       *)
+(*  LICENSE file in the root directory of this source tree.           *)
+(*                                                                    *)
+(**********************************************************************)
+
+(* DOT backend *)
+
+open Printf
+   
+type dot_config = {
+    mutable labeled_edges: bool;
+    mutable show_indexes: bool;
+    mutable local_param_box_shape: string;
+    mutable input_param_box_shape: string;
+    mutable srcsnk_box_shape: string;
+    mutable slotted_boxes: bool;
+    mutable show_wire_annots: bool;
+    mutable rank_dir: string
+  }
+
+let cfg = {
+  labeled_edges = true;
+  show_indexes = false;
+  local_param_box_shape = "invhouse";
+  input_param_box_shape = "invtriangle";
+  srcsnk_box_shape = "cds";
+  slotted_boxes = true;
+  show_wire_annots = false;
+  rank_dir = "LR"
+}
+
+let output_box ch b =
+  let open Ir in
+  let is_input io = io.io_direction = "IN" in
+  let is_output io = io.io_direction = "OUT" in
+  let ioslots ios = Misc.string_of_list (function io -> "<" ^ io.io_name ^ ">" ^ io.io_name) "|" ios in
+  match b.n_kind with
+  | "actor" ->
+      if cfg.slotted_boxes then
+        fprintf ch "%s [shape=record,style=rounded,label=\"<id>%s|{{%s}|{%s}}\"];\n"
+          b.n_name
+          b.n_name
+          (ioslots (List.filter is_input b.n_ios))
+          (ioslots (List.filter is_output b.n_ios))
+      else
+        fprintf ch "%s [shape=box,style=rounded,label=\"%s\"];\n"  b.n_name b.n_name
+  (* | GraphB ->
+   *       fprintf ch "n%d [shape=box,label=\"%s\"];\n" i  bid
+   * | SourceB | SinkB -> 
+   *    fprintf ch "n%d [shape=%s,label=\"%s\"];\n"
+   *      i
+   *      cfg.srcsnk_box_shape
+   *      bid
+   * | LocalParamB -> 
+   *    fprintf ch "n%d [shape=%s,label=\"%s\n(%s)\"];\n"
+   *      i
+   *      cfg.local_param_box_shape
+   *      bid
+   *      bval
+   * | InParamB -> 
+   *    fprintf ch "n%d [shape=%s,label=\"%s\"];\n"
+   *      i
+   *      cfg.input_param_box_shape
+   *      bid
+   * | DummyB ->  (\* Should not occur *\)
+   *     fprintf ch "n%d [shape=box,style=dotted,label=\"%s\"];\n" i "dummy" *)
+  | _ -> ()
+
+let output_wire ch w =
+  let open Ir in
+  let style = match w.e_kind with "config" -> "dashed" | _ -> "plain" in
+  let slot s p = match p with "" -> s | _ -> s ^ ":" ^ p in
+  match cfg.labeled_edges with
+  | true ->
+     fprintf ch "%s -> %s [label=\"%s\"; style=%s];\n" (slot w.e_source w.e_sourceport) (slot w.e_target w.e_targetport) w.e_type style
+  | false ->
+     fprintf ch "%s -> %s [style=%s];\n" (slot w.e_source w.e_sourceport) (slot w.e_target w.e_targetport) style
+
+let output ch boxes wires = 
+  fprintf ch "digraph g {\n";
+  fprintf ch "rankdir=%s;\n" cfg.rank_dir;
+  List.iter (output_box ch) boxes;
+  List.iter (output_wire ch) wires;
+  fprintf ch "}\n"
+
+let dump fname p =
+  let open Ir in
+  let oc = open_out fname  in
+  output oc p.p_nodes p.p_edges;
+  close_out oc;
+  printf "Generated file %s\n" fname
---
+++ tools/pi2hcl/ir.ml
@@ -0,0 +1,202 @@
+(**********************************************************************)
+(*                                                                    *)
+(*              This file is part of the HOCL package                 *)
+(*                                                                    *)
+(*  Copyright (c) 2019-present, Jocelyn SEROT (jocelyn.serot@uca.fr). *)
+(*                     All rights reserved.                           *)
+(*                                                                    *)
+(*  This source code is licensed under the license found in the       *)
+(*  LICENSE file in the root directory of this source tree.           *)
+(*                                                                    *)
+(**********************************************************************)
+
+type key = string
+type value = string
+
+type node_desc = {
+    n_id: string;
+    n_name: string;
+    n_kind: string;
+    n_period: string;
+    n_desc: string;
+    n_ports: port_desc list;
+    n_init_fn: string;
+    n_loop_fn: string;
+    n_ios: io_desc list;
+    mutable n_mark: mark (* for topological sorting *)
+  }
+    
+and port_desc = {
+    pt_name: string;
+    pt_kind: string;
+    pt_expr: string;
+    pt_annotation: string;
+  }
+
+and io_desc = {
+    io_name: string;
+    io_direction: string;
+    io_type: string;
+    io_isConfig: string;
+  }
+               
+and mark = Unmarked | TemporaryMark | PermanentMark
+                                    
+type edge_desc = {
+    e_id: string;
+    e_kind: string;
+    e_type: string;
+    e_source: string;
+    e_sourceport: string;
+    e_target: string;
+    e_targetport: string
+  }
+
+type program_desc = {
+    p_name: string;
+    p_nodes: node_desc list;
+    p_edges: edge_desc list;
+  }
+                 
+
+let rem_url ((_,name),v) = (name,v)
+
+let mk_edge_id i = "e" ^ string_of_int i
+let mk_node_id i = "n" ^ string_of_int i
+                 
+let mk_edge i (attrs, _) = mk_edge_id (i+1), List.map rem_url attrs
+
+let node_info = function `El _ -> true | `Data _ -> false
+                                                       
+let mk_node i (attrs, subnodes) = mk_node_id (i+1), List.map rem_url attrs, List.filter node_info subnodes
+                       
+let rec get_data l = match l with
+  | [] -> raise Not_found
+  | `Data d :: _ -> d 
+  | _ :: rest -> get_data rest
+
+let rec mk_node_desc (id,attrs,nodes) =
+  let name = List.assoc "id" attrs in
+  let kind = List.assoc "kind" attrs in
+  match kind with
+    "actor" -> 
+    { n_id = id;    
+      n_name = name;
+      n_kind = kind;
+      n_period = List.assoc "period" attrs;
+      n_desc = nodes |> Ezxmlm.member_with_attr "data" |> snd |> get_data;
+      n_ports = nodes |> Ezxmlm.members_with_attr "port" |> List.map fst |> List.map (List.map rem_url)
+                    |> List.map mk_port_desc;
+      n_init_fn =
+        (try nodes |> Ezxmlm.member_with_attr "init"  |> fst |> List.map rem_url |> List.assoc "name"
+        with Ezxmlm.Tag_not_found _ -> ""); 
+      n_loop_fn = nodes |> Ezxmlm.member_with_attr "loop" |> fst |> List.map rem_url |> List.assoc "name";
+      n_ios = nodes |> Ezxmlm.member_with_attr "loop" |> snd |> Ezxmlm.members_with_attr "param"
+              |> List.map fst |> List.map (List.map rem_url) |> List.map mk_io_desc;
+      n_mark = Unmarked; }
+  | _ ->
+    { n_id = id;    
+      n_name = name;
+      n_kind = kind;
+      n_period = "";
+      n_desc = "";
+      n_ports = [];
+      n_init_fn = "";
+      n_loop_fn = "";
+      n_ios = [];
+      n_mark = Unmarked; }
+
+and mk_port_desc l =
+  let lookup k = try List.assoc k l with Not_found -> "" in
+  { pt_name = lookup "name";
+    pt_kind = lookup "kind";
+    pt_expr = lookup "expr";
+    pt_annotation = lookup "annotation" }
+
+and mk_io_desc l =
+  let lookup k = try List.assoc k l with Not_found -> "" in
+  { io_name = lookup "name";
+    io_direction = lookup "direction";
+    io_type = lookup "type";
+    io_isConfig = lookup "isConfig" }
+  
+let is_param_inp io = io.io_direction = "IN" && io.io_isConfig = "true"
+let is_param_outp io = io.io_direction = "OUT" && io.io_isConfig = "true"
+let is_data_inp io = io.io_direction = "IN" && io.io_isConfig = "false"
+let is_data_outp io = io.io_direction = "OUT" && io.io_isConfig = "false"
+
+let string_of_io io = io.io_name ^ ": " ^ io.io_type
+                    
+let mk_edge_desc (id,l) =
+  let lookup k = try List.assoc k l with Not_found -> "" in
+  { e_id = id;
+    e_kind = lookup "kind";
+    e_type = lookup "type";
+    e_source = lookup "source";
+    e_sourceport = lookup "sourceport";
+    e_target = lookup "target";
+    e_targetport = lookup "targetport" }
+
+
+exception No_matching_edge of string * string * string
+exception Several_matching_edges of string * string * string
+exception No_matching_node of string * string
+exception Several_matching_nodes of string * string
+
+let find_inp_edge edges (target,targetport) = 
+  match List.filter (fun e -> e.e_target = target && e.e_targetport = targetport) edges with
+  | [] -> raise (No_matching_edge ("Ir.find_inp_edge", target, targetport))
+  | [e] -> e
+  | _ -> raise (Several_matching_edges ("Ir.find_inp_edge", target, targetport))
+                            
+let find_outp_edge edges (source,sourceport) = 
+  match List.filter (fun e -> e.e_source = source && e.e_sourceport = sourceport) edges with
+  | [] -> raise (No_matching_edge ("Ir.find_outp_edge", source, sourceport))
+  | [e] -> e
+  | _ -> raise (Several_matching_edges ("Ir.find_outp_edge", source, sourceport))
+                       
+let find_target_node nodes e =
+  match List.filter (fun n -> n.n_name = e.e_target) nodes with
+  | [] -> raise (No_matching_node ("Ir.find_target_node", e.e_target))
+  | [n] -> n
+  | _ -> raise (Several_matching_nodes ("Ir.find_target_node", e.e_target))
+      
+let succs edges nodes n =
+  let mk_edge_spec n io = n.n_name, io.io_name in
+  List.filter is_data_outp n.n_ios
+  |> List.map (mk_edge_spec n)
+  |> List.map (find_outp_edge edges)
+  |> List.map (find_target_node nodes)
+
+exception CyclicGraph of string
+
+let topological_sort edges nodes =    (* DFS-based alogorithm *)
+  let acc = ref [] in
+  let rec visit n =
+    match n.n_mark with
+    | PermanentMark -> ()
+    | TemporaryMark -> raise (CyclicGraph n.n_name)
+    | Unmarked ->
+       begin
+         n.n_mark <- TemporaryMark;
+         List.iter visit (succs edges nodes n);
+         n.n_mark <- PermanentMark;
+         acc := n :: !acc 
+       end in
+  let rec iter () = 
+    match List.find_opt (fun n -> n.n_mark = Unmarked) nodes with
+    | None -> !acc
+    | Some n -> visit n; iter () in
+  iter ()
+               
+let from_file f =
+  let ic = open_in f in
+  let _, xml = Ezxmlm.from_channel ic in
+  let g = Ezxmlm.member  "graphml" xml |> Ezxmlm.member "graph" in
+  let name = g |> Ezxmlm.member_with_attr "data" |> snd |> get_data in
+  let nodes = g |> Ezxmlm.members_with_attr "node" |> List.mapi mk_node |> List.map mk_node_desc  in
+  let edges = g |> Ezxmlm.members_with_attr "edge" |> List.mapi mk_edge |> List.map mk_edge_desc in
+  { p_name = name;
+    p_nodes = topological_sort edges nodes;
+    p_edges = edges }
+                            
