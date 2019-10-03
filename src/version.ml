228a3a4d12021c6d3200a5996f09d27513d6f8e9
jserot <jocelyn.serot@uca.fr>
2019-10-03 16:13:09 +0200

    Track src/version.ml

---
+++ src/version.ml
@@ -0,0 +1,14 @@
+(**********************************************************************)
+(*                                                                    *)
+(*              This file is part of the HOCL package                 *)
+(*                                                                    *)
+(*  Copyright (c) 2019-present, Jocelyn SEROT (jocelyn.serot@uca.fr). *)
+(*                     All rights reserved.                           *)
+(*                                                                    *)
+(*  This source code is licensed under the license found in the       *)
+(*  LICENSE file in the root directory of this source tree.           *)
+(*                                                                    *)
+(**********************************************************************)
+
+(* This file is generated *)
+let version = "0.2"
